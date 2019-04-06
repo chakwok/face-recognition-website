f993b60d881de73bd7ecbb79a378a6d08322a559
akwok <andyk2014@gmail.com>
2019-04-06 23:02:34 +0800

    add face recognition box

--- src/FaceRecognition/FaceRecognition.js
+++ src/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
     <div className='center ma'> 
         <div className='absolute mt2'>
-            <img alt='' src={imageUrl} width='500px' height='auto' />
+            <img id='inputImage' alt='' src={imageUrl} width='500px' height='auto' />
+            <div className='boundBox' style={{ right: box.rightCol, bottom: box.bottomRow, left: box.leftCol, top: box.topRow }}></div>
+
         </div>
     </div>
 );}
